98fb56617d250926f547aa99bf065ad57ad38a85
abd3lrahman <abdomohamedelsayed2@gmail.com>
2026-07-05 15:13:52 +0300

    feat: initialize project structure with splash screen assets and home screen skeleton

--- lib/features/auth/presintation/screens/home_screen.dart
+++ lib/features/auth/presintation/screens/home_screen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
-    );
+    return Scaffold(body: Center(child: Text("Home")));
   }
 }
